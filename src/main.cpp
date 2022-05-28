38bc099a94db05da9ad81c3f7234d14b4c0163f0
nejcgalof <galof.nejc@gmail.com>
2022-05-28 19:36:20 +0000

    Feat: Add configure file from cmake for versioning

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include <internal_use_only/config.hpp>
+
 int main()
 {
-  std::cout << "hey world\n";
+  std::cout << "Program: " << demo_project::cmake::project_name << "\n";
+  std::cout << "Version: " << demo_project::cmake::project_version << "." << demo_project::cmake::git_hash << "\n";
   return 0;
 }
